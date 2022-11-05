a811d28b25d87f3855e02eb4421937d3b81cd0f5
Konrad Perlicki <k.perlicki@ideo.pl>
2022-11-05 22:12:30 +0100

    Refactored index file and build script

--- server/.tools/build.sh
+++ server/.tools/build.sh
@@ -21,17 +21,21 @@ echo "${YELLOW}Starting containers for '${BOLD}${DOCKER_PREFIX%$'\r'}'...${RESET
 docker-compose up -d
 
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
-echo -e "${BOLD}${YELLOW}Installing server dependencies...${RESET}\n"
+echo "${BOLD}${YELLOW}Installing server dependencies...${RESET}\n"
 docker exec -i "${DOCKER_PREFIX%$'\r'}"_node npm install
 
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
-echo -e "${BOLD}${YELLOW}Installing front-end dependencies...${RESET}\n"
+echo "${BOLD}${YELLOW}Installing front-end dependencies...${RESET}\n"
 docker exec -i "${DOCKER_PREFIX%$'\r'}"_react npm install
 
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 
 #import database from other script
 server/.tools/db/import.sh
+
+echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
+echo -e "${BOLD}${YELLOW}Running all migrations...${RESET}\n"
+docker exec -i "${DOCKER_PREFIX%$'\r'}"_node npm run run-migrations
 
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 echo "${YELLOW}The panel is available at: ${BOLD}${GREEN}http://localhost:${DOCKER_REACT_PORT%$'\r'}/${RESET}"
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,50 +7,72 @@ import User from "./entity/User";
 import AppDataSource from "./data-source";
 import redis from "./redis";
 
-const app = express();
+class App {
+  private app = express();
 
-/* Test */
-app.use(cors());
-app.get("/", async (req: Request, res: Response) => {
-  await User.create({
-    firstName: "test",
-    lastName: "test",
-    password: "test",
-    email: "test2",
-  }).save();
-  res.json([
-    {
-      id: "1",
-      title: "Bookasdax sRsevisewasd: The Bear & The Nightingale",
-    },
-    {
-      id: "2",
-      title: "Gaxmxaxxxxaea Resviews: Pokemon Brillian Diamond",
-    },
-    {
-      id: process.env.DOCKER_NODE_PORT || 5000,
-      title: "Showsdbbbbaaasaaa Review: Alice in Borderland",
-    },
-  ]);
-});
+  constructor() {
+    this.initMiddlewares();
+    this.connectDB();
+  }
 
-app.listen(process.env.DOCKER_NODE_PORT, async () => {
-  await redis.connect();
+  private initMiddlewares(): void {
+    this.app.use(cors());
+  }
 
-  let retries = 5;
-  while (retries) {
-    try {
-      AppDataSource.initialize();
-      break;
-    } catch (err) {
-      retries -= 1;
-      console.log(err);
-      console.log(`retries left: ${retries}`);
+  private async connectDB(): Promise<void> {
+    let retries = 5;
+    while (retries) {
+      try {
+        AppDataSource.initialize();
 
-      //Wait 5 seconds
-      await new Promise((res) => setTimeout(res, 5000));
+        await this.startServer();
+
+        break;
+      } catch (err) {
+        retries -= 1;
+        console.log(err);
+        console.log(`retries left: ${retries}`);
+
+        //Wait 5 seconds
+        await new Promise((res) => setTimeout(res, 5000));
+      }
     }
   }
 
-  console.log("listening for requests on port " + process.env.DOCKER_NODE_PORT);
-});
+  private async startServer(): Promise<void> {
+    this.app.get("/", async (req: Request, res: Response) => {
+      await User.create({
+        firstName: "test",
+        lastName: "test",
+        password: "test",
+        email: "test2",
+      }).save();
+      res.json([
+        {
+          id: "1",
+          title: "Bookasdax sRsevisewasd: The Bear & The Nightingale",
+        },
+        {
+          id: "2",
+          title: "Gaxmxaxxxxaea Resviews: Pokemon Brillian Diamond",
+        },
+        {
+          id: process.env.DOCKER_NODE_PORT || 5000,
+          title: "Showsdbbbbaaasaaa Review: Alice in Borderland",
+        },
+      ]);
+    });
+
+    await this.startRedis();
+
+    this.app.listen(process.env.DOCKER_NODE_PORT, async () => {
+      console.log("listening for requests on port " + process.env.DOCKER_NODE_PORT);
+    });
+  }
+
+  private async startRedis(): Promise<void> {
+    await redis.connect();
+  }
+}
+/* Test */
+new App();
